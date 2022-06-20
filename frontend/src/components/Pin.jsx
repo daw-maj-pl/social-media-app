7577bd1ab9d1aa3589b27823dc9f2399e11a1771
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-06-20 12:05:04 +0200

    Add link to the specific image

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -92,6 +92,19 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                 </button>
               )}
             </div>
+            <div className=" flex justify-between items-center gap-2 w-full">
+              {destination && (
+                <a
+                  href={destination}
+                  target="_blank"
+                  className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md"
+                  rel="noreferrer"
+                >
+                  <BsFillArrowUpRightCircleFill />
+                  {destination?.slice(8, 20)}...
+                </a>
+              )}
+            </div>
           </div>
         )}
       </div>
