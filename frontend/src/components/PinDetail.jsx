6aa65c6260de9add81f9248a5dce0e0d427ae9c0
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-06-24 16:45:41 +0200

    Create PinDetail component and fetchPinsDetails function

---
+++ frontend/src/components/PinDetail.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { client, urlFor } from '../client';
+import MasonryLayout from './MasonryLayout';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import Spinner from './Spinner';
+
+const PinDetail = ({ user }) => {
+  const [pins, setPins] = useState();
+  const [pinDetail, setPinDetail] = useState();
+  const [comment, setComment] = useState('');
+  const [addingComment, setAddingComment] = useState(false);
+  const { pinId } = useParams();
+
+  const fetchPinDetails = () => {
+    let query = pinDetailQuery(pinId);
+
+    if (query) {
+      client.fetch(`${query}`).then(data => {
+        setPinDetail(data[0]);
+
+        if (data[0]) {
+          query = pinDetailMorePinQuery(data[0]);
+          client.fetch(query).then(res => {
+            setPins(res);
+          });
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchPinDetails();
+  }, [pinId]);
+
+  if (!pinDetail) {
+    return <Spinner message="Loading pin..." />;
+  }
+
+  return <div></div>;
+};
+
+export default PinDetail;
