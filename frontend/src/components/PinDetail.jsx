b2822a394fa36d46df80e215ac28cc6e67f48793
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-06-26 20:53:21 +0200

    Add comments section

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -83,6 +83,25 @@ const PinDetail = ({ user }) => {
           />
           <p className="font-bold">{pinDetail?.postedBy.userName}</p>
         </Link>
+        <h2 className="mt-5 text-2xl">Comments</h2>
+        <div className="max-h-370 overflow-y-auto">
+          {pinDetail?.comments?.map(item => (
+            <div
+              className="flex gap-2 mt-5 items-center bg-white rounded-lg"
+              key={item.comment}
+            >
+              <img
+                src={item.postedBy?.image}
+                className="w-10 h-10 rounded-full cursor-pointer"
+                alt="user-profile"
+              />
+              <div className="flex flex-col">
+                <p className="font-bold">{item.postedBy?.userName}</p>
+                <p>{item.comment}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
