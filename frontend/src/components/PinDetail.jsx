3f9c4007a6091919cd604fbfa9bfe0fe0617e757
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-06-27 19:47:38 +0200

    Add abitity to add new comments

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -35,6 +35,29 @@ const PinDetail = ({ user }) => {
     fetchPinDetails();
   }, [pinId]);
 
+  const addComment = () => {
+    if (comment) {
+      setAddingComment(true);
+
+      client
+        .patch(pinId)
+        .setIfMissing({ comments: [] })
+        .insert('after', 'comments[-1]', [
+          {
+            comment,
+            _key: uuidv4(),
+            postedBy: { _type: 'postedBy', _ref: user._id }
+          }
+        ])
+        .commit()
+        .then(() => {
+          fetchPinDetails();
+          setComment('');
+          setAddingComment(false);
+        });
+    }
+  };
+
   if (!pinDetail) {
     return <Spinner message="Loading pin..." />;
   }
@@ -101,6 +124,29 @@ const PinDetail = ({ user }) => {
               </div>
             </div>
           ))}
+        </div>
+        <div className="flex flex-wrap mt-6 gap-3">
+          <Link to={`/user-profile/${user._id}`}>
+            <img
+              src={user.image}
+              className="w-10 h-10 rounded-full cursor-pointer"
+              alt="user-profile"
+            />
+          </Link>
+          <input
+            className=" flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+            type="text"
+            placeholder="Add a comment"
+            value={comment}
+            onChange={e => setComment(e.target.value)}
+          />
+          <button
+            type="button"
+            className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+            onClick={addComment}
+          >
+            {addingComment ? 'Posting...' : 'Post'}
+          </button>
         </div>
       </div>
     </div>
