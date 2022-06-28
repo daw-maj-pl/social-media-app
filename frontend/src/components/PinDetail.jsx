8b8099ed3bbad86643d077be58744a47ef4326b6
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-06-28 10:17:47 +0200

    Show related posts

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -63,93 +63,105 @@ const PinDetail = ({ user }) => {
   }
 
   return (
-    <div
-      className="flex xl:flex-row flex-col m-auto bg-white"
-      style={{ maxWidth: '1500px', borderRadius: '32px' }}
-    >
-      <div className="flex justify-center items-center md:items-start flex-initial">
-        <img
-          className="rounded-t-3xl rounded-b-lg"
-          src={pinDetail?.image && urlFor(pinDetail?.image).url()}
-          alt="user-post"
-        />
-      </div>
-      <div className="w-full p-5 flex-1 xl:min-w-620">
-        <div className="flex items-center justify-between">
-          <div className="flex gap-2 items-center">
-            <a
-              href={`${pinDetail.image.asset.url}?dl=`}
-              download
-              className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
-            >
-              <MdDownloadForOffline />
+    <>
+      <div
+        className="flex xl:flex-row flex-col m-auto bg-white"
+        style={{ maxWidth: '1500px', borderRadius: '32px' }}
+      >
+        <div className="flex justify-center items-center md:items-start flex-initial">
+          <img
+            className="rounded-t-3xl rounded-b-lg"
+            src={pinDetail?.image && urlFor(pinDetail?.image).url()}
+            alt="user-post"
+          />
+        </div>
+        <div className="w-full p-5 flex-1 xl:min-w-620">
+          <div className="flex items-center justify-between">
+            <div className="flex gap-2 items-center">
+              <a
+                href={`${pinDetail.image.asset.url}?dl=`}
+                download
+                className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+              >
+                <MdDownloadForOffline />
+              </a>
+            </div>
+            <a href={pinDetail.destination} target="_blank" rel="noreferrer">
+              {pinDetail.destination?.slice(8)}
             </a>
           </div>
-          <a href={pinDetail.destination} target="_blank" rel="noreferrer">
-            {pinDetail.destination?.slice(8)}
-          </a>
-        </div>
-        <div>
-          <h1 className="text-4xl font-bold break-words mt-3">
-            {pinDetail.title}
-          </h1>
-          <p className="mt-3">{pinDetail.about}</p>
-        </div>
-        <Link
-          to={`/user-profile/${pinDetail?.postedBy._id}`}
-          className="flex gap-2 mt-5 items-center bg-white rounded-lg "
-        >
-          <img
-            src={pinDetail?.postedBy.image}
-            className="w-10 h-10 rounded-full"
-            alt="user-profile"
-          />
-          <p className="font-bold">{pinDetail?.postedBy.userName}</p>
-        </Link>
-        <h2 className="mt-5 text-2xl">Comments</h2>
-        <div className="max-h-370 overflow-y-auto">
-          {pinDetail?.comments?.map(item => (
-            <div
-              className="flex gap-2 mt-5 items-center bg-white rounded-lg"
-              key={item.comment}
-            >
+          <div>
+            <h1 className="text-4xl font-bold break-words mt-3">
+              {pinDetail.title}
+            </h1>
+            <p className="mt-3">{pinDetail.about}</p>
+          </div>
+          <Link
+            to={`/user-profile/${pinDetail?.postedBy._id}`}
+            className="flex gap-2 mt-5 items-center bg-white rounded-lg "
+          >
+            <img
+              src={pinDetail?.postedBy.image}
+              className="w-10 h-10 rounded-full"
+              alt="user-profile"
+            />
+            <p className="font-bold">{pinDetail?.postedBy.userName}</p>
+          </Link>
+          <h2 className="mt-5 text-2xl">Comments</h2>
+          <div className="max-h-370 overflow-y-auto">
+            {pinDetail?.comments?.map(item => (
+              <div
+                className="flex gap-2 mt-5 items-center bg-white rounded-lg"
+                key={item.comment}
+              >
+                <img
+                  src={item.postedBy?.image}
+                  className="w-10 h-10 rounded-full cursor-pointer"
+                  alt="user-profile"
+                />
+                <div className="flex flex-col">
+                  <p className="font-bold">{item.postedBy?.userName}</p>
+                  <p>{item.comment}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-wrap mt-6 gap-3">
+            <Link to={`/user-profile/${user._id}`}>
               <img
-                src={item.postedBy?.image}
+                src={user.image}
                 className="w-10 h-10 rounded-full cursor-pointer"
                 alt="user-profile"
               />
-              <div className="flex flex-col">
-                <p className="font-bold">{item.postedBy?.userName}</p>
-                <p>{item.comment}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-wrap mt-6 gap-3">
-          <Link to={`/user-profile/${user._id}`}>
-            <img
-              src={user.image}
-              className="w-10 h-10 rounded-full cursor-pointer"
-              alt="user-profile"
+            </Link>
+            <input
+              className=" flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+              type="text"
+              placeholder="Add a comment"
+              value={comment}
+              onChange={e => setComment(e.target.value)}
             />
-          </Link>
-          <input
-            className=" flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
-            type="text"
-            placeholder="Add a comment"
-            value={comment}
-            onChange={e => setComment(e.target.value)}
-          />
-          <button
-            type="button"
-            className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
-            onClick={addComment}
-          >
-            {addingComment ? 'Posting...' : 'Post'}
-          </button>
+            <button
+              type="button"
+              className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+              onClick={addComment}
+            >
+              {addingComment ? 'Posting...' : 'Post'}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {pins?.length > 0 && (
+        <h2 className="text-center font-bold text-2xl mt-8 mb-4">
+          More like this
+        </h2>
+      )}
+      {pins ? (
+        <MasonryLayout pins={pins} />
+      ) : (
+        <Spinner message="Loading more pins" />
+      )}
+    </>
   );
 };
 
