4a721dc4a4e408de3648c10d7663f53c9f687851
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-06-28 18:23:03 +0200

    Create UserProfile component and add logout functionality and banner and user images

---
+++ frontend/src/components/UserProfile.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+import {
+  userQuery,
+  userCreatedPinsQuery,
+  userSavedPinsQuery
+} from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+const UserProfile = () => {
+  const [user, setUser] = useState();
+  const [pins, setPins] = useState();
+  const [text, setText] = useState('Created');
+  const [activeBtn, setActiveBtn] = useState('created');
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  const User =
+    localStorage.getItem('user') !== 'undefined'
+      ? JSON.parse(localStorage.getItem('user'))
+      : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(userId);
+    client.fetch(query).then(data => {
+      setUser(data[0]);
+    });
+  }, [userId]);
+
+  const logout = () => {
+    localStorage.clear();
+
+    navigate('/login');
+  };
+
+  if (!user) return <Spinner message="Loading profile..." />;
+
+  return (
+    <div className="relative pb-2 h-full justify-center items-center">
+      <div className="flex flex-col pb-5">
+        <div className="relative flex flex-col mb-7">
+          <div className="flex flex-col justify-center items-center">
+            <img
+              className=" w-full h-370 2xl:h-510 shadow-lg object-cover"
+              src="https://source.unsplash.com/1600x900/?nature,photography,technology"
+              alt="banner-pic"
+            />
+            <img
+              className="rounded-full w-20 h-20 -mt-10 shadow-xl object-cover"
+              src={user.image}
+              alt="user-pic"
+            />
+            <h1 className="font-bold text-3xl text-center mt-3">
+              {user.userName}
+            </h1>
+            <div className="absolute top-0 z-1 right-0 p-2">
+              {userId === User.sub && (
+                <button
+                  type="button"
+                  className=" bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+                  onClick={logout}
+                >
+                  <AiOutlineLogout color="red" fontSize={21} />
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
