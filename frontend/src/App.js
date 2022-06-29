da55ea36059e08f1b304b50258ce08102ce220c5
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-06-29 16:14:32 +0200

    Navigate not logged in user to the login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './containers/Home';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User =
+      localStorage.getItem('user') !== 'undefined'
+        ? JSON.parse(localStorage.getItem('user'))
+        : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
